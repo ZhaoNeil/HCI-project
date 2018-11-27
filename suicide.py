7d433a2e6a8a28ee2c0beb30d4304e5c9ba632ee
Neil <zhao.yuxuan@foxmail.com>
2018-11-27 15:30:15 +0100

    add change

--- suicide.py
+++ suicide.py
@@ -11,7 +11,7 @@ identifier = data.sheets()[1]
 population = data.sheets()[2]
 i=-1
 
-workbook = xlsxwriter.Workbook('test.xlsx')
+workbook = xlsxwriter.Workbook('suicide population.xlsx')
 worksheet = workbook.add_worksheet()
 for n in rate.col_values(1):
 	i=i+1
@@ -19,7 +19,10 @@ for n in rate.col_values(1):
 		for col in range(0,identifier.ncols):
 			if n==identifier.cell_value(row,col):
 				m=population.cell_value(row,col)*rate.cell_value(i,2)
-				# //divide 86400 by the output from the above calculation
+				m=m/100000
 				m=m/365
 				m=86400/m
-				worksheet.write_number(row,col,m)
+				if m>0:
+					worksheet.write_number(row,col,m)
+				
+suicide = xlrd.open_workbook('/Users/zhaoyuxuan/Desktop/suicide population.xlsx')
